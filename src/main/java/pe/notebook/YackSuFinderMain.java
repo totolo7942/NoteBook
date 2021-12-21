451719b5749fdf02535cef489c5898cdfcae7509
백태성(Taesung Baek)/CX개발팀/11ST <aaron.baek@sk.com>
2021-12-21 22:49:59 +0900

    yacksu add

---
+++ src/main/java/pe/notebook/YackSuFinderMain.java
@@ -0,0 +1,16 @@
+package pe.notebook;
+
+
+public class YackSuFinderMain {
+
+  public static void main(String[] args) {
+    int num =12;
+
+    for (int i = 1; i <= num; i++) {
+      if (num % i == 0) {
+        System.out.println(i);
+      }
+    }
+  }
+
+}
