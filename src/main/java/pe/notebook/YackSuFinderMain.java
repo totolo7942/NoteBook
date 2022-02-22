80f0751b61bd2f5b9c03169ac503c48835418995
백태성(Taesung Baek)/쿠폰/정산개발팀/11ST <aaron.baek@sk.com>
2022-02-22 11:45:01 +0900

    add java primary type check, decimal

--- src/main/java/pe/notebook/YackSuFinderMain.java
+++ src/main/java/pe/notebook/YackSuFinderMain.java
@@ -1,9 +1,17 @@
 package pe.notebook;
 
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.function.Predicate;
+
 public class YackSuFinderMain {
 
   public static void main(String[] args) {
+
+    String decimal = "0.1";
+    System.out.println(zeroRate.test(decimal));
+
     int num =12;
 
     for (int i = 1; i <= num; i++) {
@@ -12,5 +20,17 @@ public class YackSuFinderMain {
       }
     }
   }
+
+  protected static Predicate<String> zeroRate = (i) -> {
+    if( StringUtils.isBlank(i))
+      return false;
+
+    double v = Double.parseDouble(i);
+    if( v % 1 != 0) {
+      return v <= 0.0;
+    }
+
+    return v <= 0;
+  };
 
 }
